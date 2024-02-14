03c40b2652942ec6d4dd1b2fe9e4b5cd5c756e2d
Savindu Supun <savindusupunsathsara@gmail.com>
2024-02-15 01:44:42 +0530

    Impl Search ✨

---
+++ src/components/search.jsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { useRouter, useSearchParams } from 'next/navigation';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+
+
+export default function Search() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  function onSubmit(e) {
+    e.preventDefault();
+
+    const val = e.target ;
+    const search = val.search ;
+    const newParams = new URLSearchParams(searchParams.toString());
+
+    if (search.value) {
+      newParams.set('q', search.value);
+    } else {
+      newParams.delete('q');
+    }
+
+    const paramsString = newParams.toString();
+  const queryString = `${paramsString.length ? '?' : ''}${paramsString}`;
+  
+
+    router.push(`/${queryString}`);
+  }
+
+  return (
+    <form onSubmit={onSubmit} className="w-max-[550px] relative w-full lg:w-80 xl:w-full">
+      <input
+        key={searchParams?.get('q')}
+        type="text"
+        name="search"
+        placeholder="Search for products..."
+        autoComplete="off"
+        defaultValue={searchParams?.get('q') || ''}
+        className="w-full rounded-lg border bg-white px-4 py-2 text-sm text-black placeholder:text-neutral-500 dark:border-neutral-800 dark:bg-transparent dark:text-white dark:placeholder:text-neutral-400"
+      />
+      <div className="absolute right-0 top-0 mr-3 flex h-full items-center">
+      <MagnifyingGlassIcon className="h-4" />
+      </div>
+    </form>
+  );
+}
