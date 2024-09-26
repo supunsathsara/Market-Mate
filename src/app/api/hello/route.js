132eb8a6d20945ca4e079b3315b816e5bbddf75f
Savindu Supun <contact@supunsathsara.com>
2024-09-27 01:37:28 +0530

    disable cache in hello route

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,6 +1,8 @@
 import { query } from "@/lib/db";
 import { NextResponse } from "next/server";
 
+export const revalidate = 0
+
 export async function GET(req, res) {
     const products = await query({
         query: "SELECT * FROM product;",
